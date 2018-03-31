58dab7311b6e529bd55f0e339564544ec3dce649
Kirill Pavlov <k@p99.io>
2018-03-31 18:44:49 +0800

    ADD: Question implementation.

---
+++ src/question.js
@@ -0,0 +1,15 @@
+export default class Question {
+  static splitBlocks (question) {
+    // Apply simple regexps first and then if candidate is not qualified - merge
+    // it with text on the left and right.
+    // NOTES: (Negative) lookbehind is not supported by JavaScript yet. It is a
+    // part of ES2018 proposal.
+    // More info about lookbehind: http://2ality.com/2017/05/regexp-lookbehind-assertions.html
+    // There is a possible way to replace lookabehind wiht lookahead on the
+    // reversed string: https://stackoverflow.com/questions/641407/javascript-negative-lookbehind-equivalent#answer-11347100
+    // return question.split(/(?<!\\)({[^}]*(?<!\\)})/g).filter(x => x)
+    return question
+      .split(/({[^}]*})/g)
+      .filter(x => x)
+  }
+}
---
+++ src/question.spec.js
@@ -0,0 +1,38 @@
+import test from 'tape'
+import Question from './question'
+
+test('splitBlocks', (t) => {
+  t.test('should split question string into block chunks simple', (st) => {
+    const question = '1 + 1 {=2}'
+    const result = Question.splitBlocks('1 + 1 {=2}')
+    st.deepEqual(result, ['1 + 1 ', '{=2}'])
+    st.equal(result.join(''), question)
+    st.end()
+  })
+
+  t.test('should split question string into block chunks', (st) => {
+    const questions = [
+      ['1 + 1 ', '{#2}', '. Sure!'],
+      ['1 + 1 ', '{#2}', '. Sure! How about 2 * 2:', '{=4 ~5}'],
+      ['String without GIFT blocks'],
+    ]
+    questions.forEach((chunks) => {
+      st.deepEqual(Question.splitBlocks(chunks.join('')), chunks)
+    })
+    st.end()
+  })
+
+  t.skip('should split escaped string into block chunks', (st) => {
+    const questions = [
+      ['Escaped opened \\{'],
+      ['Escaped opened \\{ }']
+      ['Escaped closed { \\}']
+      ['Escaped both \\{ \\}'],
+      ['Escaped both ', '{ \\} \\{ }'],
+    ]
+    questions.forEach((chunks) => {
+      st.deepEqual(Question.splitBlocks(chunks.join('')), chunks)
+    })
+    st.end()
+  })
+})
