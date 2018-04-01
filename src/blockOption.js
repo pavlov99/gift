9968e0b28f938c12d62ed2df7611e2ff73292fff
Kirill Pavlov <k@p99.io>
2018-04-01 12:26:26 +0800

    REF: :shirt: fix linter warning for BlockOption.

--- src/blockOption.js
+++ src/blockOption.js
@@ -1,4 +1,4 @@
-/** Single option of the block.*/
+/** Single option of the block. */
 export default class BlockOption {
   /**
    * @param {?string} prefix. Either '=' or '~', optional.
@@ -6,52 +6,55 @@ export default class BlockOption {
    * @param {string} value, possible answer to the question.
    * @param {?string} feedback optional feedback to the user.
    */
-  constructor ({ prefix, credit, value, feedback }) {
+  constructor({
+    prefix, credit, value, feedback,
+  }) {
     if (value === undefined) {
-      throw new Error(`Invalid value '${value}', should not be empty`)
+      throw new Error(`Invalid value '${value}', should not be empty`);
     }
-    this.value = value
+    this.value = value;
 
-    if (!(prefix === undefined || prefix == '=' || prefix == '~')) {
-      throw new Error(`Invalid prefix value: ${prefix}. Use [undefined, '~', '=']`)
+    if (!(prefix === undefined || prefix === '=' || prefix === '~')) {
+      throw new Error(`Invalid prefix value: ${prefix}. Use [undefined, '~', '=']`);
     }
-    this.prefix = prefix
+    this.prefix = prefix;
 
     if (credit !== undefined) {
-      if (isNaN(parseFloat(credit))) {
-        throw new Error(`Invalid credit value '${credit}'. Should be a number.`)
+      this.credit = parseFloat(credit);
+      if (!this.credit) {
+        throw new Error(`Invalid credit value '${credit}'. Should be a number.`);
       }
-      this.credit = parseFloat(credit)
       if (this.credit > 1 || this.credit < -1) {
-        throw new Error(`Invalid credit value '${this.credit}'. Should be in [-1, 1].`)
+        throw new Error(`Invalid credit value '${this.credit}'. Should be in [-1, 1].`);
       }
     } else {
-      this.credit = undefined
+      this.credit = undefined;
     }
 
-    this.feedback = feedback
+    this.feedback = feedback;
   }
 
-  toString () {
-    const prefix = this.prefix ? this.prefix : ''
-    const credit = this.credit ? `%${this.credit * 100}%` : ''
-    const feedback = this.feedback ? `#${this.feedback}` : ''
-    return `${prefix}${credit}${this.value}${feedback}`
+  toString() {
+    const prefix = this.prefix ? this.prefix : '';
+    const credit = this.credit ? `%${this.credit * 100}%` : '';
+    const feedback = this.feedback ? `#${this.feedback}` : '';
+    return `${prefix}${credit}${this.value}${feedback}`;
   }
 
-  static fromString (option) {
-    const re = /^([=~])?(%-?\d{1,3}%)?([^=~#]*)(#[^=~]*)?$/
-    const groups = option.match(re)
+  static fromString(option) {
+    const re = /^([=~])?(%-?\d{1,3}%)?([^=~#]*)(#[^=~]*)?$/;
+    const groups = option.match(re);
     if (groups) {
       const credit = groups[2] ?
-        parseFloat(groups[2].slice(1, -1)) / 100 : undefined
-      const feedback = groups[4] ? groups[4].slice(1) : undefined
+        parseFloat(groups[2].slice(1, -1)) / 100 : undefined;
+      const feedback = groups[4] ? groups[4].slice(1) : undefined;
       return new BlockOption({
         prefix: groups[1],
         credit,
         value: groups[3],
-        feedback
-      })
+        feedback,
+      });
     }
+    return undefined;
   }
 }
