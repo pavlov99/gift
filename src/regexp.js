827ae6702f6c55ce846af3d06c5a3646b2ba73c8
Kirill Pavlov <k@p99.io>
2018-06-11 23:17:50 +0800

    DEL: :fire: remove unused regexp

--- src/regexp.js
+++ src/regexp.js
@@ -1,3 +1,2 @@
 export const NUMBER = /([+-]?\d+(\.\d+)?)([eE][+-]?\d+)?/;
 export const INTERVAL = new RegExp(`${NUMBER.source}((:${NUMBER.source})|(\\.\\.${NUMBER.source}))?`);
-export const OPTION = /[=~](%\d{2}%)?[^=~]*(#[^=~]*)?/;
